cd0870f4fe7173d4b7b30cdff71060dc28df1389
leedhm92 <leedhm92@ajou.ac.kr>
2017-12-22 14:01:04 +0900

    code clean

--- Capstone/src/Draw.java
+++ Capstone/src/Draw.java
@@ -1,45 +1,48 @@
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.awt.*;
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Draw extends JPanel implements ActionListener {
 
 	public static ArrayList<Integer> list = new ArrayList<Integer>();
 
-	Timer time = new Timer(100, (ActionListener) this);
-	int a = 0;
-	int b = 3;
-	int s = 7;
-	int size = 0;
-
+	Timer time = new Timer(50, (ActionListener) this);
+	int a = 0; //list 시작 변수
+	int b = 3; //list 끝 변수
+	int s = 5; //draw 이미지 크기
+	int size = 0; //pathList size
+	
 	public void animateLine(Graphics2D g2d) {
 		list = Algoritm.getPathList();
 		if (b == 3)
 			size = list.size();
-		// System.out.println(b);
 		if (b < size)
 			b += 3;
-		if (b == size)
-			System.out.println("그리기 완료");
-		
-		synchronized (this) {
-			for (a = 0; a < b; a += 3) {
+		if(b == size)
+			System.out.println("Draw End");
+	
+		synchronized(this){
+		for (a = 0; a < b; a += 3) {
 
-				if (list.get(a + 3) == 0) {
-					g2d.setColor(Color.black);
-					g2d.drawLine(list.get(a + 1) * s, list.get(a + 2) * s, list.get(a + 4) * s, list.get(a + 5) * s);
-					time.start();
-				}
+			if (list.get(a + 3) == 0) {
+				g2d.setColor(Color.black);
+				g2d.drawLine(list.get(a + 1)*s, list.get(a + 2)*s, list.get(a + 4)*s, list.get(a + 5)*s);
+				time.start();
+			}
 
-				else if (list.get(a + 3) != 0) {
-					g2d.setColor(Color.red);
-					g2d.drawLine(list.get(a + 1) * s, list.get(a + 2) * s, list.get(a + 4) * s, list.get(a + 5) * s);
-					time.start();
-				}
-				if (b == size) {
-					time.stop();
-					notify();
+			else if (list.get(a + 3) != 0) {
+				g2d.setColor(Color.red);
+				g2d.drawLine(list.get(a + 1)*s, list.get(a + 2)*s, list.get(a + 4)*s, list.get(a + 5)*s);
+				time.start();
+			}
+			if(b == size){
+				time.stop();
+				notify();
 				}
 			}
 		}
@@ -47,11 +50,8 @@ public class Draw extends JPanel implements ActionListener {
 
 	public void actionPerformed(ActionEvent arg0) {
 		repaint();
-		// System.out.println("그리는중");
-		// System.out.println(b);
-		// System.out.println(b);
-	}
-
+		}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
